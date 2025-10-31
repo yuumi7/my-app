3fd35b5133ab4ffda0daece047bdcb39c0f89f7b
yuumi7 <alisasergeevna777@gmail.com>
2025-10-31 16:29:14 +0500

    components   css and js

---
+++ src/components/app-filter/app-filter.js
@@ -0,0 +1,21 @@
+import "./app-filter.css";
+
+const AppFilter = () => {
+  return (
+    <div className="btn-group">
+      <button className="btn btn-light" type="button">
+        Все сотрудники
+      </button>
+
+      <button className="btn btn-outline-light" type="button">
+        На повышение
+      </button>
+
+      <button className="btn btn-outline-light" type="button">
+        З/П более 1000$
+      </button>
+    </div>
+  );
+};
+
+export default AppFilter;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,9 @@
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
+
 import "./app.css";
 
 function App() {
@@ -8,7 +12,10 @@ function App() {
       <AppInfo />
       <div className="search-panel">
         <SearchPanel />
+        <AppFilter />
       </div>
+      <EmployeesList />
+      <EmployeesAddForm />
     </div>
   );
 }
