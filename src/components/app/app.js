7a00e21b60750cdf0105669d3262e384859bdc4c
yuumi7 <alisasergeevna777@gmail.com>
2025-11-02 15:34:11 +0500

    rise and increase 1

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,9 +13,9 @@ class App extends Component {
     super(props);
     this.state = {
       data: [
-        { name: "John C.", salary: 800, increase: false, id: 1 },
-        { name: "Alex M.", salary: 3000, increase: true, id: 2 },
-        { name: "Carl W.", salary: 5000, increase: false, id: 3 },
+        { name: "John C.", salary: 800, increase: false, rise: true, id: 1 },
+        { name: "Alex M.", salary: 3000, increase: true, rise: false, id: 2 },
+        { name: "Carl W.", salary: 5000, increase: false, rise: false, id: 3 },
       ],
     };
     this.maxId = 4;
@@ -34,6 +34,7 @@ class App extends Component {
       name,
       salary,
       increase: false,
+      rise: false,
       id: this.maxId++,
     };
     this.setState(({ data }) => {
@@ -42,6 +43,19 @@ class App extends Component {
         data: newArr,
       };
     });
+  };
+
+  onToggleIncrease = (id) => {
+    this.setState(({ data }) => {
+      const index = data.findIndex((elem) => elem.id === id);
+
+      const old = data[index];
+      const newItem = {};
+    });
+  };
+
+  onToggleRise = (id) => {
+    console.log(`Rise this ${id}`);
   };
 
   render() {
@@ -54,7 +68,12 @@ class App extends Component {
           <AppFilter />
         </div>
 
-        <EmployeesList data={this.state.data} onDelete={this.deleteItem} />
+        <EmployeesList
+          data={this.state.data}
+          onDelete={this.deleteItem}
+          onToggleIncrease={this.onToggleIncrease}
+          onToggleRise={this.onToggleRise}
+        />
         <EmployeesAddForm onAdd={this.addItem} />
       </div>
     );
