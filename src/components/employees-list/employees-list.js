ea3ade3eb54d8ad572562720ad02655e0e38dfb8
yuumi7 <alisasergeevna777@gmail.com>
2025-11-02 15:34:30 +0500

    rise and increase 1

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,7 +2,7 @@ import EmployeesListItem from "../employees-list-item/employess-list-item";
 
 import "./employees-list.css";
 
-const EmployeesList = ({ data, onDelete }) => {
+const EmployeesList = ({ data, onDelete, onToggleIncrease, onToggleRise }) => {
   const elements = data.map((item) => {
     const { id, ...itemProps } = item;
     return (
@@ -10,6 +10,8 @@ const EmployeesList = ({ data, onDelete }) => {
         key={id}
         {...itemProps}
         onDelete={() => onDelete(id)}
+        onToggleIncrease={() => onToggleIncrease(id)}
+        onToggleRise={() => onToggleRise(id)}
       />
     );
   });
