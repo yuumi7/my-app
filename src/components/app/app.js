5efb3f82e55303a6ac42fa78f0e29980348085e0
yuumi7 <alisasergeevna777@gmail.com>
2025-11-04 14:45:00 +0500

    edit form and info

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,9 +13,15 @@ class App extends Component {
     super(props);
     this.state = {
       data: [
-        { name: "John C.", salary: 800, increase: false, rise: true, id: 1 },
-        { name: "Alex M.", salary: 3000, increase: true, rise: false, id: 2 },
-        { name: "Carl W.", salary: 5000, increase: false, rise: false, id: 3 },
+        { name: "Alex N.", salary: 800, increase: false, rise: true, id: 1 },
+        { name: "Andrey K.", salary: 3000, increase: true, rise: false, id: 2 },
+        {
+          name: "Polina P.",
+          salary: 5000,
+          increase: false,
+          rise: false,
+          id: 3,
+        },
       ],
     };
     this.maxId = 4;
@@ -45,24 +51,23 @@ class App extends Component {
     });
   };
 
-  onToggleIncrease = (id) => {
-    this.setState(({ data }) => {
-      const index = data.findIndex((elem) => elem.id === id);
-
-      const old = data[index];
-      const newItem = {};
-    });
-  };
-
-  onToggleRise = (id) => {
-    console.log(`Rise this ${id}`);
+  onToggleProp = (id, prop) => {
+    this.setState(({ data }) => ({
+      data: data.map((item) => {
+        if (item.id === id) {
+          return { ...item, [prop]: !item[prop] };
+        }
+        return item;
+      }),
+    }));
   };
 
   render() {
+    const employees = this.state.data.length;
+    const increased = this.state.data.filter((item) => item.increase).length;
     return (
       <div className="app">
-        <AppInfo />
-
+        <AppInfo employees={employees} increased={increased} />
         <div className="search-panel">
           <SearchPanel />
           <AppFilter />
@@ -71,8 +76,7 @@ class App extends Component {
         <EmployeesList
           data={this.state.data}
           onDelete={this.deleteItem}
-          onToggleIncrease={this.onToggleIncrease}
-          onToggleRise={this.onToggleRise}
+          onToggleProp={this.onToggleProp}
         />
         <EmployeesAddForm onAdd={this.addItem} />
       </div>
